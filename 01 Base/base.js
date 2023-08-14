40be5f98c7eda804e2350fbc37dfea1c769dea41
Inna018 <122346373+Inna018@users.noreply.github.com>
2023-08-14 23:27:50 +0300

    add confirm() example.

--- 01 Base/base.js
+++ 01 Base/base.js
@@ -1,1 +1,31 @@
-console.log("Message from the separate js file!");
+//task 1
+let yourLogin = prompt("Enter your login:");
+let yourEmail = prompt("Enter your email:");
+let yourPassword = prompt("Enter your password:");
+
+alert(
+  `Dear ${yourLogin}, your email is ${yourEmail}, your password is ${yourPassword}`
+);
+
+//task 2
+let dataTypes = {
+  String: typeof "JavaScript",
+  Number: 71,
+  Boolean: true,
+  Undefined: undefined,
+  Null: null,
+  Big_Int: 123n,
+};
+
+//task 3
+const second = 60;
+let secH = 60 * second;
+let secD = 24 * secH;
+let secM = 31 * secD;
+
+//task 4
+let isAdult = confirm("Are you of legal age?");
+
+//task 5
+let myName = "Inna";
+alert(`My name is ${myName}`);
